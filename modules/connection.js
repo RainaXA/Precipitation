8a8598b24d5414e9f77ccd1b86a5adedd66e7be3
RainaXA <rainaxa8@gmail.com>
2022-09-16 23:23:15 -0700

    2.2-dev

--- modules/connection.js
+++ modules/connection.js
@@ -1,12 +1,12 @@
 client.on('reconnecting', function() {
-    log("Precipitation is currently disconnected and attempting to reconnect.", logging.warning, 1);
+    log("Precipitation is currently disconnected and attempting to reconnect.", logging.warning, "CONNECTION");
 });
 
 client.on('disconnect', function() {
-    log("\nPrecipitation has disconnected and will not reconnect.", logging.error, 3);
-    log("You'll need to restart Precipitation to reconnect.", logging.output, 2)
+    log("\nPrecipitation has disconnected and will not reconnect.", logging.error, "CONNECTION");
+    log("You'll need to restart Precipitation to reconnect.", logging.output, "CONNECTION")
 });
 
 client.on('resume', function(replayed) {
-  log("Precipitation has reconnected - " + replayed + " events were replayed.", logging.success, 1)
+  log("Precipitation has reconnected - " + replayed + " events were replayed.", logging.success, "CONNECTION")
 })
